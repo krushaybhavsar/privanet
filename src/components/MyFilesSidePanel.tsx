f37ff8c95ed270a5d0e7e21f05f0d1dd925599ba
Krushay Bhavsar <krushaybhavsar@gmail.com>
2023-01-08 04:14:01 -0500

    Added side panel and upload file button

---
+++ src/components/MyFilesSidePanel.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./css/MyFilesSidePanel.css";
+
+type MyFilesSidePanelProps = {};
+
+const MyFilesSidePanel = (props: MyFilesSidePanelProps) => {
+  return <div className="my-files-side-panel"></div>;
+};
+
+export default MyFilesSidePanel;
--- src/screens/fragments/MyFilesFragment.tsx
+++ src/screens/fragments/MyFilesFragment.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { UploadCloudIcon } from "../../assets/UploadCloudIcon";
+import MyFilesSidePanel from "../../components/MyFilesSidePanel";
 import "./css/MyFilesFragment.css";
 
 const MyFilesFragment = () => {
-  return <div>My Files Fragment</div>;
+  return (
+    <div className="my-files-fragment">
+      <div className="my-files-fragment__left">
+        <div className="my-files-fragment__header">
+          <h1>{"Hey there!"}</h1>
+          <div className="my-files-fragment__header__upload">
+            <button className="my-files-fragment__header__upload__btn">
+              <UploadCloudIcon className="my-files-fragment__header__upload__icon" />
+              <p>{"Upload"}</p>
+            </button>
+          </div>
+        </div>
+        <div className="my-files-fragment__content"></div>
+      </div>
+      <MyFilesSidePanel />
+    </div>
+  );
 };
 
 export default MyFilesFragment;
